1b80c7ca581aac5beba39e14042a10ce1ac153ff
Brookins Consulting <info@brookinsconsulting.com>
2012-03-25 16:18:42 -0500

    Added: Initial import of bcsimpleliteralhtmlsettings extension

---
+++ ezinfo.php
@@ -0,0 +1,38 @@
+<?php
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: BC Simple Literal HTML Settings
+// SOFTWARE RELEASE: 0.3.0
+// COPYRIGHT NOTICE: Copyright (C) 1999-2012 eZ Systems AS, 1999-2012 Brookins Consulting
+// SOFTWARE LICENSE: GNU General Public License v2.0 (or later)
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public Licenseas
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+class paliteralhtmlInfo
+{
+    static function info()
+    {
+        return array( 'Name' => 'BC Simple Literal HTML : Settings',
+                      'Version' => '0.3.0',
+                      'Copyright' => 'Copyright (C) 1999-2012 eZ Systems AS, Brookins Consulting',
+                      'License' => 'GNU General Public License v2.0 (or later)'
+                     );
+    }
+}
+?>
---
+++ settings/content.ini.append.php
@@ -0,0 +1,14 @@
+<?php /* #?ini charset="utf-8"?
+# eZ publish configuration file for content
+
+# BC: Enable ezxml literal tag html class
+[literal]
+# AvailableClasses[]
+# The class 'html' is disabled by default because it gives editors the
+# possibility to insert html and javascript code in XML blocks.
+# Don't enable the 'html' class unless you really trust all users who has
+# privileges to edit objects containing XML blocks.
+# AvailableClasses[]=html
+# CustomAttributes[]
+
+*/ ?>
